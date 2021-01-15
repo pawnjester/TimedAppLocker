b8f922671a40a8e702e3a6a88542adf0ade16766
pawnjester <ebukeokonji@gmail.com>
2021-01-16 00:56:45 +0100

    - setup project to follow clean code

---
+++ app/src/main/java/com/example/timedapplocker/ApplicationClass.kt
@@ -0,0 +1,15 @@
+package com.example.timedapplocker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import net.danlew.android.joda.JodaTimeAndroid
+
+@HiltAndroidApp
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        JodaTimeAndroid.init(this)
+    }
+}
